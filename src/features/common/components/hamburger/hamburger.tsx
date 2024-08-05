d9a77bb2b1c16c2e4db47116516f800d5269f21a
Kevin Kim <kkim1401@github.com>
2024-08-05 18:08:58 -0400

    add menu

--- src/features/common/components/hamburger/hamburger.tsx
+++ src/features/common/components/hamburger/hamburger.tsx
@@ -1,13 +1,18 @@
+import { MouseEventHandler } from 'react';
 import styles from './hamburger.module.css';
 import { concatClassNames } from '@/utils';
 
 type HamburgerProps = {
-  className: string;
+  className?: string;
+  onClick?: MouseEventHandler;
 };
 
 export default function Hamburger(props: HamburgerProps) {
   return (
-    <div className={concatClassNames(styles.container, props.className)}>
+    <div
+      className={concatClassNames(styles.container, props.className)}
+      onClick={(e) => props?.onClick?.(e)}
+    >
       <span className={styles.line} />
       <span className={styles.line} />
       <span className={styles.line} />
--- src/features/common/components/topbar/topbar.tsx
+++ src/features/common/components/topbar/topbar.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Link from 'next/link';
 import { Hamburger } from '../hamburger';
 import styles from './topbar.module.css';
+import { concatClassNames } from '@/utils';
 
 export default function Topbar() {
+  const [showMenu, setShowMenu] = useState(false);
+  const handleHamburgerClick = () => setShowMenu(true);
+
   return (
     <nav className={styles.container}>
-      <Hamburger className={styles.hamburger} />
+      <Hamburger onClick={handleHamburgerClick} />
+      <ui
+        className={concatClassNames(
+          styles.menu,
+          showMenu && styles.menuSelected
+        )}
+      >
+        <li>
+          <Link href='/settings'>Settings</Link>
+        </li>
+      </ui>
     </nav>
   );
 }
