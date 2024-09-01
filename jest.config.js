0118f7dfddaf98744f8e3d685e277f00f49c0c53
Kevin Kim <kkim1401@github.com>
2024-09-01 19:03:42 -0400

    add eslint and prettier

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const nextJest = require('next/jest.js');
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
 // Add any custom config to be passed to Jest
 const config = {
@@ -12,9 +12,9 @@ const config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  }
-}
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(config)
+module.exports = createJestConfig(config);
