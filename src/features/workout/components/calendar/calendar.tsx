06a1a123bcd8656ade9cfbeef9e393c9f3ff9402
Kevin Kim <kkim1401@github.com>
2024-09-01 19:03:23 -0400

    add current day to calendar

--- src/features/workout/components/calendar/calendar.tsx
+++ src/features/workout/components/calendar/calendar.tsx
@@ -1,5 +1,7 @@
-'use client';
+import { concatClassNames } from '@/utils';
 import styles from './calendar.module.css';
+
+const dayNames = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
 function addDays(date: Date, days: number) {
   const newDate = new Date(date);
@@ -8,7 +10,6 @@ function addDays(date: Date, days: number) {
 }
 
 export function getDaysByDayName(date: Date): { [index: string]: number } {
-  const dayNames = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
   const currDayIndex = date.getDay();
   return dayNames.reduce((acc, curr, index) => {
     return {
@@ -18,27 +19,50 @@ export function getDaysByDayName(date: Date): { [index: string]: number } {
   }, {});
 }
 
+function getCurrentDayName(date: Date) {
+  const currDayIndex = date.getDay();
+  return dayNames[currDayIndex];
+}
+
 function CalendarDay({
   dayName,
   dayNumber,
+  isCurrent,
 }: {
   dayName: string;
   dayNumber: number;
+  isCurrent?: boolean;
 }) {
   return (
     <div className={styles.day}>
-      <div className={styles.dayName}>{dayName}</div>
-      <div className={styles.dayNumber}>{dayNumber}</div>
+      <div className='subtitle2'>{dayName}</div>
+      <div
+        className={concatClassNames(
+          styles.dayNumber,
+          isCurrent && styles.current,
+          'body1'
+        )}
+      >
+        {dayNumber}
+      </div>
     </div>
   );
 }
 
 export default function Calendar() {
-  const daysByDayName = getDaysByDayName(new Date());
+  const date = new Date();
+  const daysByDayName = getDaysByDayName(date);
+  const currentDayName = getCurrentDayName(date);
+
   return (
     <section className={styles.container}>
       {Object.entries(daysByDayName).map(([dayName, dayNumber]) => (
-        <CalendarDay key={dayName} dayName={dayName} dayNumber={dayNumber} />
+        <CalendarDay
+          key={dayName}
+          dayName={dayName}
+          dayNumber={dayNumber}
+          isCurrent={currentDayName === dayName}
+        />
       ))}
     </section>
   );
