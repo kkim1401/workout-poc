85bc3a931aeaff747df5fe1ccad4228729787a4d
Kevin Kim <ykk3416@gmail.com>
2024-09-04 19:03:02 -0400

    add button states for text

--- src/features/common/components/button/button.tsx
+++ src/features/common/components/button/button.tsx
@@ -6,21 +6,26 @@ export default function Button({
   children,
   className,
   onClick,
-  variant = 'default',
+  type = 'button',
+  variant = 'contained',
 }: {
   children: ReactNode;
   className?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
-  variant?: 'default' | 'text';
+  type?: HTMLButtonElement['type'];
+  variant?: 'contained' | 'raised' | 'text';
 }) {
   return (
     <button
       className={concatClassNames(
         styles.container,
-        variant === 'text' && 'headline5',
+        variant === 'contained' && styles.contained,
+        variant === 'raised' && styles.raised,
+        variant === 'text' && styles.text,
         className
       )}
       onClick={onClick}
+      type={type}
     >
       {children}
     </button>
