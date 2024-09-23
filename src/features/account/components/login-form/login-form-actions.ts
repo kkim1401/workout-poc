799d348b019854ae4eef5595c82d77a6723785db
Kevin Kim <ykk3416@gmail.com>
2024-09-22 20:49:53 -0400

    remove password

--- src/features/account/components/login-form/login-form-actions.ts
+++ src/features/account/components/login-form/login-form-actions.ts
@@ -4,37 +4,14 @@ import { createClient } from '@/utils/supabase/server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-export async function login(formData: FormData) {
+export async function signInWithEmail(formData: FormData) {
   const supabase = createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
+  const { error } = await supabase.auth.signInWithOtp({
+    // type-casting here for convenience
+    // in practice, you should validate your inputs
     email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  };
-
-  const { error } = await supabase.auth.signInWithPassword(data);
-
-  if (error) {
-    redirect('/error');
-  }
-
-  revalidatePath('/', 'layout');
-  redirect('/');
-}
-
-export async function signup(formData: FormData) {
-  const supabase = createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  };
-
-  const { error } = await supabase.auth.signUp(data);
+  });
 
   if (error) {
     redirect('/error');
--- src/features/account/components/login-form/login-form.tsx
+++ src/features/account/components/login-form/login-form.tsx
@@ -1,6 +1,6 @@
 import { Card, TextField } from '@/features/common/components';
 import clsx from 'clsx';
-import { login, signup } from './login-form-actions';
+import { signInWithEmail } from './login-form-actions';
 import styles from './login-form.module.css';
 
 type LoginFormProps = {
@@ -11,9 +11,7 @@ export default function LoginForm({ className }: LoginFormProps) {
   return (
     <Card as='form' className={clsx(styles.container, className)}>
       <TextField label='Email' name='email' type='email' required />
-      <TextField label='Password' name='password' type='password' required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
+      <button formAction={signInWithEmail}>Sign In</button>
     </Card>
   );
 }
