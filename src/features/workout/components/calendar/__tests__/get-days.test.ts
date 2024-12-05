1167c47f8c9219679fffe70d328e416d196defff
Kevin Kim <ykk3416@gmail.com>
2024-12-05 18:48:18 -0500

    fix mock date issue again again again

--- src/features/workout/components/calendar/__tests__/get-days.test.ts
+++ src/features/workout/components/calendar/__tests__/get-days.test.ts
@@ -1,8 +1,16 @@
 import { getDaysByDayName } from '../calendar';
 
-jest.useFakeTimers().setSystemTime(new Date(new Date('2020-01-06T00:00:00Z')));
-
 describe('getDaysByDayName', () => {
+  beforeEach(() => {
+    jest
+      .useFakeTimers()
+      .setSystemTime(new Date(new Date('2020-01-06T00:00:00Z')));
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
   it('getDaysByDayName should return a dictionary of days', () => {
     expect(getDaysByDayName(new Date())).toEqual({
       Su: 5,
--- src/utils/__tests__/add-days.test.ts
+++ src/utils/__tests__/add-days.test.ts
@@ -1,8 +1,16 @@
 import addDays from '../add-days';
 
-jest.useFakeTimers().setSystemTime(new Date(new Date('2020-01-06T00:00:00Z')));
-
 describe('addDays', () => {
+  beforeEach(() => {
+    jest
+      .useFakeTimers()
+      .setSystemTime(new Date(new Date('2020-01-06T00:00:00Z')));
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
   it('should add days and return new Date instance if second argument is positive', () => {
     expect(addDays(new Date(), 3).toDateString()).toBe('Thu Jan 09 2020');
   });
