752bc7a9a9900d2ab2e00fe6e4cca80c3af5ad6a
Kevin Kim <ykk3416@gmail.com>
2025-04-06 18:40:03 -0400

    add fixed size to text field

--- src/features/common/components/text-field/text-field.tsx
+++ src/features/common/components/text-field/text-field.tsx
@@ -5,24 +5,25 @@ import styles from './text-field.module.css';
 type TextFieldProps = ComponentPropsWithoutRef<'input'> & {
   error?: string;
   label: string;
+  fixedSize?: boolean;
 };
 
 const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
-  ({ className, error, label, ...rest }, ref) => {
+  ({ className, error, fixedSize = true, label, ...rest }, ref) => {
     const errorId = className + '_error';
     const hasError = Boolean(error);
     return (
-      <label className={clsx('subtitle2', styles.container, className)}>
+      <label className={clsx(styles.container, className)}>
         {label}
         <input
           aria-invalid={hasError}
           {...(hasError && { 'aria-errormessage': errorId })}
-          className={clsx('body1', styles.input)}
+          className={clsx(styles.input, fixedSize && styles.fixedSize)}
           ref={ref}
           {...rest}
         />
         {hasError && (
-          <strong className={clsx('subtitle2', styles.error)} id={errorId}>
+          <strong className={styles.error} id={errorId}>
             {error}
           </strong>
         )}
